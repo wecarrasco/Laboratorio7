4965ff62e3ff462f449899fc379683d8457649b5
Walther Carrasco <waemcame@hotmail.es>
2015-06-12 16:03:44 -0600

    Falta Herencia

---
+++ investigador.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "persona.hpp"
+
+using namespace std;
+
+class Investigador{
+public:
+	Investigador(int casosAtendidos, int casosCerrados, int casosSinResolver);
+	~Investigador();
+	void setcasosAtendidos(int casosAtendidos);
+	int getcasosAtendidos() const;
+	void setcasosCerrados(int casosCerrados);
+	int getcasosCerrados() const;
+	void setcasosSinResolver(int casosSinResolver);
+	int getcasosSinResolver() const;
+
+protected:
+	int casosAtendidos;
+	int casosCerrados;
+	int casosSinResolver;
+}
