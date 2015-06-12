a6f9548939fabecf4b39da450cf13023b049b351
Walther Carrasco <waemcame@hotmail.es>
2015-06-12 15:31:58 -0600

    Falta Herencia

---
+++ personaladministrativo.cpp
@@ -0,0 +1,19 @@
+#include "personaladministrativo.hpp"
+
+PersonalAdministrativo::PersonalAdministrativo(string clave, string puesto){
+	this->clave = clave;
+	this->puesto = puesto;
+}
+PersonalAdministrativo::~PersonalAdministrativo(){}
+void PersonalAdministrativo::setClave(string clave){
+	this->clave = clave;
+}
+PersonalAdministrativo::string getClave() const{
+	return this->clave;
+}
+void PersonalAdministrativo::setPuesto(string puesto){
+	this->puesto = puesto;
+}
+string PersonalAdministrativo::getPuesto() const{
+	return this->puesto;
+}
--- personaladministrativo.hpp
+++ personaladministrativo.hpp
@@ -1,1 +1,20 @@
 #pragma once
+
+#include "persona.hpp"
+
+#include <string>
+
+using namespace std;
+
+class PersonalAdministrativo{
+public:
+	PersonalAdministrativo(string clave, string puesto);
+	~PersonalAdministrativo();
+	void setClave(string clave);
+	string getClave() const;
+	void setPuesto(string puesto);
+	string getPuesto() const;
+private:
+	string clave;
+	string puesto;
+};
